ecb813f4d2b8faec1b80500cfaff7d72ecd7d7ba
Brittany Byrd <bbyrd227@uw.edu>
2018-05-01 23:09:33 -0700

    Added AuctionTest and added constructors/methods
    I added constructors for Auction class and Organization class. I also added methods to help with testing to Auction Class.

--- src/Model/Auction.java
+++ src/Model/Auction.java
@@ -4,18 +4,47 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Auction {
+	
 	private Date startDate;
 	private Date endDate;
 	private int maxItemsPerBidder;
 	private static final int DEFAULT_MAX_ITEMS = 5;
-	private Organization forOrgainization;
+	private Organization forOrganization;
 	private Date creationDate;
 	private String location; // make a location object?
 	private ArrayList<Item> inventory;
+	
+	public Auction(Date testStartDate, Date testEndDate,
+				   Date testCreationDate,
+				   int theMaxItemsPerBidder, Organization theOrganization) {
+		
+		startDate = testStartDate;
+		endDate = testEndDate;
+		creationDate = testCreationDate;
+		maxItemsPerBidder = theMaxItemsPerBidder;
+		forOrganization = theOrganization;
+		inventory = new ArrayList<>();
+		
+		
+	}
+				   
 	
 	
 	public void addItem(Item item) {
 		// check if legal?
 		inventory.add(item);
 	}
+	
+	public int getInventoryCount() {
+		return inventory.size();
+	}
+	
+	//Checks if the auction is at the default max capacity.
+	//Returns true if inventory size equals default capacity.
+	public boolean isAuctionAtMaxCapacity() {
+		
+		return getInventoryCount() == DEFAULT_MAX_ITEMS;
+			
+	}
+	
 }
