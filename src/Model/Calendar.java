5376b9a66f0caebc2fc770baeeb2c1eccce622d9
David Monaghan <dmon323@uw.edu>
2018-05-06 18:40:26 -0700

    fixed merge conflicts

--- src/Model/Calendar.java
+++ src/Model/Calendar.java
@@ -1,13 +1,13 @@
 package Model;
-<<<<<<< HEAD
+
 import java.util.ArrayList;
-=======
+
 import java.io.Serializable;
->>>>>>> 31a8e073fe97aab63950523703274b55b2bd1d93
+
 import java.time.LocalDate;
 import java.io.Serializable;
 
-<<<<<<< HEAD
+
 
 
 public class Calendar implements Serializable {
@@ -15,22 +15,14 @@ public class Calendar implements Serializable {
 	private ArrayList<Auction> auctions;
 	private int MAX_DAYS = 2;
 
-=======
-public class Calendar implements Serializable {
-	private static final long serialVersionUID = 7055729347303320715L;
-	private Auction[] auctions;
-	private int MAX_DAYS = 2;
->>>>>>> 31a8e073fe97aab63950523703274b55b2bd1d93
+
+
 	private LocalDate currentDate;
 	
 	public Calendar(LocalDate theCurrentDate) {
 		currentDate = theCurrentDate;
 	}
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 31a8e073fe97aab63950523703274b55b2bd1d93
 	
 	public boolean checkDate(LocalDate theDate){
 		int num = 0;
