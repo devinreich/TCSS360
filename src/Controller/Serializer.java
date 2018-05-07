79f6ab2bde03847adfef01746e5568fd98edc164
David Monaghan <dmon323@uw.edu>
2018-05-07 10:31:03 -0700

    Started to test Serializing Calendar

--- src/Controller/Serializer.java
+++ src/Controller/Serializer.java
@@ -4,8 +4,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+
 import Model.Bidder;
+import Model.Calendar;
 import Model.PhoneNumber;
+
 
 public class Serializer {
 	
@@ -16,7 +19,7 @@ public class Serializer {
 	public static void main(String[] args) {
 		Bidder bidder = new Bidder("John Doe", "johnny", new PhoneNumber(555, 555, 5555), "3326 Hidden Lookout\n" + 
 				"Elephant Point, Washington, 98865-8265, US");
-		
+		Calendar calendar;
 		try {
 	         FileOutputStream fileOut =
 	         new FileOutputStream("src/SerializedObjects/johnny.ser");
@@ -28,6 +31,18 @@ public class Serializer {
 	      } catch (IOException i) {
 	         i.printStackTrace();
 	      }
+		calendar = new Calendar();
+//		try {
+//			FileOutputStream fileOut =
+//			new FileOutputStream("src/SerializedObjects/calendar.ser");
+//			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+//			out.writeObject(calendar);
+//			out.close();
+//			fileOut.close();
+//			System.out.printf("Serialized data is saved");
+//			} catch (IOException i) {
+//			   i.printStackTrace();
+//			}
 	}
 
 }
