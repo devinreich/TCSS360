d524a5a9743ea4c2b656f046054c82f260d91c3c
YunhanTu <36247811+YunhanTu@users.noreply.github.com>
2018-05-06 19:58:35 -0700

    ·

--- src/Model/Calendar.java
+++ src/Model/Calendar.java
@@ -3,7 +3,7 @@ package Model;
 import java.util.ArrayList;
 
 import java.io.Serializable;
-
+import java.time.temporal.ChronoUnit;
 import java.time.LocalDate;
 import java.io.Serializable;
 
@@ -60,7 +60,27 @@ public class Calendar implements Serializable {
 		return(futureAuctions);
 	}
 	
+	public boolean checkForUpcomingDays(Auction theAuction) {
+		boolean result = false;
+		Auction thePioneer = auctions.get(0);
+		for(int i = 0; i < auctions.size()-1; i++) {
+			if(auctions.get(i).getStartDate().isBefore(thePioneer.getStartDate()))
+				thePioneer = auctions.get(i);
+		}
+		if(thePioneer.getStartDate().getYear() - theAuction.getStartDate().getYear() == 0) {
+			if (thePioneer.getStartDate().getMonthValue() - theAuction.getStartDate().getMonthValue() < 2 ){
+				result = true;
+			}
+			else if (thePioneer.getStartDate().getMonthValue() - theAuction.getStartDate().getMonthValue() == 2) {
+				long DaysBetween = ChronoUnit.DAYS.between(thePioneer.getStartDate(), theAuction.getStartDate());
+				if(DaysBetween <= MAX_UPCOMING_AUCTIONS_DAYS){
 
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
 	public boolean checkForUpComingAuctionNumber() {
 		return auctions.size() <= 25;
 	}
